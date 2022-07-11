8601c45a29070eddd1027bcdf2091f812e21d1d8
Kais <kais@Halas-MacBook-Pro.local>
2022-07-11 12:56:30 +0100

    changes to api links

--- app/Http/Controllers/ViewController.php
+++ app/Http/Controllers/ViewController.php
@@ -12,39 +12,11 @@ use stdClass;
 
 class Viewcontroller extends Controller
 {
-    public $mappedSura;
     public $request;
 
     public function __construct(Request $request)
     {
         $this->request = $request;
-    }
-
-    public function viewSearchResults()
-    {
-        $query = ($this->request->searchQuery);
-        $result = [];
-        $mappedSura = (array)json_decode(file_get_contents(storage_path('decoded_suras/المصحف_data.json')));
-        $mappedSura['suraString'] = '';
-        $querySplit = preg_split('//u', $query, -1, PREG_SPLIT_NO_EMPTY);
-        for ($i = 0; $i < count($querySplit); $i++) {
-            $letterOcc = (array)$mappedSura['letterOccurrences'];
-            // $letterIndexes = (array)$mappedSura['letterIndexes'];
-            $result['letterOccurrences'][$querySplit[$i]] = $letterOcc[$querySplit[$i]];
-            // $result['letterIndexes'][$querySplit[$i]] = $letterIndexes[$querySplit[$i]];
-        }
-        $wordOcc = (array)$mappedSura['wordOccurrences'];
-        $wordIndexes = (array)$mappedSura['wordIndexes'];
-        foreach ($wordOcc as $word => $occ) {
-
-            if (mb_ereg($query, $word) !== false) {
-
-                $result['wordOccurrences'][$word] = $occ;
-                $result['wordIndexes'][$word] = $wordIndexes[$word];  
-            }
-        }
-
-        return $result;
     }
 
     public function viewSuraDetails()
@@ -57,16 +29,6 @@ class Viewcontroller extends Controller
         return json_encode($obj);
 
     }
-    public function viewSuraCharts()
-    {
-        $suraName = $this->request->suraName;
-        return file_get_contents(storage_path('categorized_suras/suras_charts/' . $suraName));
-    }
-
-    public function allSurasData()
-    {
-        return file_get_contents(storage_path('categorized_suras/search_basics/allSurasData'));
-    }
     public function viewOneQuranFile()
     {
         return file_get_contents(storage_path('categorized_suras/search_basics/oneQuranFile'));
@@ -75,77 +37,9 @@ class Viewcontroller extends Controller
     {
         return (file_get_contents(storage_path('allVersesWithTashkeel')));
     }
-    public function viewVersesBasics()
-    {
-        $suraName = $this->request->suraName;
-        return file_get_contents(storage_path('categorized_suras/verses_basics/' . $suraName));
-    }
-
-    public function viewSuraBasics()
-    {
-        $suraName = $this->request->suraName;
-        return file_get_contents(storage_path('categorized_suras/suras_basics/' . $suraName));
-    }
-    public function viewSuraText()
-    {
-        $suraName = $this->request->suraName;
-        return file_get_contents(storage_path('categorized_suras/suras_text/' . $suraName));
-    }
-
-    public function viewSuraElement()
-    {
-        $suraName = $this->request->suraName;
-        $mappedSura = file_get_contents(storage_path('decoded_suras/' . $suraName . '_data.json'));
-        $mappedSura = json_decode($mappedSura);
-        $dataType = $this->request->dataType;
-        $this->suraMap[$dataType] = $mappedSura->{$dataType};
-        return $this->parseArrayToObj($this->suraMap[$dataType]);
-    }
-    public function parseArrayToObj($results)
-    {
-        $tmp = [];
-        $index = 0;
-        if (is_object($results)) {
-            foreach ($results as $key => $value) {
-                $tmp[$key] = $value;
-                $index++;
-            }
-            return $tmp;
-        } else    return json_encode($results);
-    }
-    public function viewVerseElement()
-    {
-        $suraName = $this->request->suraName;
-        $mappedSura = file_get_contents(storage_path('decoded_suras/' . $suraName . '_data.json'));
-        $mappedSura = json_decode($mappedSura);
-        $dataType = $this->request->dataType;
-        dd('Needs work!');
-        return $this->parseVerseObject($dataType, $mappedSura);
-    }
-
     public function viewQuranIndex()
     {
         $quranIndex = file_get_contents(storage_path('quranIndexWithData'));
         return $quranIndex;
-    }
-
-    public function viewFileNames()
-    {
-        $quranIndex = file_get_contents(storage_path('quranIndex'));
-        return $quranIndex;
-    }
-
-    public function viewQuranString()
-    {
-        $file = file_get_contents(storage_path('allSurasDataRaw'));
-        $file = json_decode($file);
-        $allQuranString = "";
-
-        foreach ($file as $value) {
-            $allQuranString .= ($value->verseText) . ',';
-        }
-        file_put_contents(storage_path('rawQuranText'), ($allQuranString));
-
-        return $allQuranString;
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -18,47 +18,48 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// Route::get('allSurasData/all', 'ViewController@allSurasData');
 
-Route::get('quran-index', 'ViewController@viewQuranIndex');
-Route::get('fileNames', 'ViewController@viewFileNames');
+// Route::get('fileNames', 'ViewController@viewFileNames');
 
-Route::get('view/verses-basics/{suraName}', 'ViewController@viewVersesBasics');
+// Route::get('view/verses-basics/{suraName}', 'ViewController@viewVersesBasics');
+
+
+// Route::get('view/sura-charts/{suraName}', 'ViewController@viewSuraCharts');
+// Route::get('view/sura-basics/{suraName}', 'ViewController@viewSuraBasics');
+// Route::get('view/sura-text/{suraName}', 'ViewController@viewSuraText');
+// Route::get('view/search/{searchQuery}', 'ViewController@viewSearchResults');
+
+// //files Categorizer
+// Route::get('categorize', 'CategorizerController@categorize');
+
+
+// // Backend calculations
+// Route::get('sura-map/{fileName}', 'CalculatorController@mapSura');
+// Route::get('verses-map/{fileName}', 'CalculatorController@mapVerses');
+// Route::get('letters-map/{fileName}', 'CalculatorController@mapLetters');
+// Route::get('sanatize', 'SanatizerController@createAllSurasFiles');
+// Route::get('SanatizerNew', 'SanatizerController@SanatizerNew');
+
+// //send quraIndex
+// Route::get('quran-index/{fileName}', 'CalculatorController@listSuras');
+// Route::get('decode-all', 'CalculatorController@runBackend');
+
+// //you have to decode-all before running Quran map
+// Route::get('quran-map/{fileName}', 'CalculatorController@mapComplete');
+// Route::get('count-letters/{suraIndex}', 'CollecterController@countLetters');
+// Route::get('calculate-sura-19/{suraName}', 'CollecterController@calculateSura19');
+
+// //Scores:
+// Route::get('words-score/{fileName}', 'ScoreController@eachWordScore');
+// Route::get('verses-score/{fileName}', 'ScoreController@eachVerseScore');
+// Route::get('verse-score/{fileName}/{verseIndex}', 'ScoreController@singleVerseScore');
+// Route::get('find-19-in-sura/{fileName}', 'ScoreController@find19InSura');
 
 
 Route::get('view/sura-details/{suraName}', 'ViewController@viewSuraDetails');
-Route::get('view/sura-charts/{suraName}', 'ViewController@viewSuraCharts');
-Route::get('view/sura-basics/{suraName}', 'ViewController@viewSuraBasics');
-Route::get('view/sura-text/{suraName}', 'ViewController@viewSuraText');
-Route::get('view/search/{searchQuery}', 'ViewController@viewSearchResults');
-Route::get('allSurasData/all', 'ViewController@viewOneQuranFile');
-Route::get('allVersesWithTashkeel', 'ViewController@viewAllVersesWithTashkeel');
+Route::get('quran-model', 'ViewController@viewOneQuranFile');
+Route::get('with-tashkeel', 'ViewController@viewAllVersesWithTashkeel');
+Route::get('quran-index', 'ViewController@viewQuranIndex');
 
 
 
-
-//files Categorizer
-Route::get('categorize', 'CategorizerController@categorize');
-
-
-// Backend calculations
-Route::get('sura-map/{fileName}', 'CalculatorController@mapSura');
-Route::get('verses-map/{fileName}', 'CalculatorController@mapVerses');
-Route::get('letters-map/{fileName}', 'CalculatorController@mapLetters');
-Route::get('sanatize', 'SanatizerController@createAllSurasFiles');
-Route::get('SanatizerNew', 'SanatizerController@SanatizerNew');
-
-//send quraIndex
-Route::get('quran-index/{fileName}', 'CalculatorController@listSuras');
-Route::get('decode-all', 'CalculatorController@runBackend');
-
-//you have to decode-all before running Quran map
-Route::get('quran-map/{fileName}', 'CalculatorController@mapComplete');
-Route::get('count-letters/{suraIndex}', 'CollecterController@countLetters');
-Route::get('calculate-sura-19/{suraName}', 'CollecterController@calculateSura19');
-
-//Scores:
-Route::get('words-score/{fileName}', 'ScoreController@eachWordScore');
-Route::get('verses-score/{fileName}', 'ScoreController@eachVerseScore');
-Route::get('verse-score/{fileName}/{verseIndex}', 'ScoreController@singleVerseScore');
-Route::get('find-19-in-sura/{fileName}', 'ScoreController@find19InSura');
